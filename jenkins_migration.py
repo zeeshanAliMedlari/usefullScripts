0ff992304ffda9685703b86d58ab28a8b706d235
Zeeshan <zeeshanmedlari@gmail.com>
2024-06-11 17:55:23 +0530

    updated path

--- jenkins_migration.py
+++ jenkins_migration.py
@@ -31,44 +31,61 @@ def switch_kube_context(context):
     print(f"Switching to {context} cluster context...")
     run_command(f"kubectl config use-context {context}")
 
-def backup_jenkins_data(namespace, pod, backup_path, config_backup_path):
+#def backup_jenkins_data(namespace, pod, backup_path, config_backup_path):
+def backup_jenkins_data(namespace, pod, backup_path):
     print(f"Backing up Jenkins data from {namespace} namespace...")
     run_command(f"kubectl -n {namespace} exec {pod} -- tar -czvf /tmp/jenkins-backup.tar.gz {backup_path}")
+
+    '''
+    # Check if the jenkins_config directory exists before attempting to back it up
+    print(f"Checking if {config_backup_path} exists in {namespace} namespace...")
+    check_command = f"kubectl -n {namespace} exec {pod} -- test -d {config_backup_path}"
     try:
+        run_command(check_command)
+        print(f"{config_backup_path} exists. Backing it up...")
         run_command(f"kubectl -n {namespace} exec {pod} -- tar -czvf /tmp/jenkins-config-backup.tar.gz {config_backup_path}")
     except subprocess.CalledProcessError:
-        print(f"Directory {config_backup_path} does not exist. Skipping config backup.")
+        print(f"{config_backup_path} does not exist. Skipping config backup.")
+    '''
 
 def copy_backups_to_local(namespace, pod):
     print("Copying backups to local machine... This might take a while.")
     run_command(f"kubectl -n {namespace} cp {pod}:/tmp/jenkins-backup.tar.gz ./jenkins-backup.tar.gz")
+    '''
     try:
         run_command(f"kubectl -n {namespace} cp {pod}:/tmp/jenkins-config-backup.tar.gz ./jenkins-config-backup.tar.gz")
     except subprocess.CalledProcessError:
         print("Config backup not found on the pod. Skipping config backup copy.")
+    '''
 
 def copy_backups_to_pod(namespace, pod):
     print(f"Copying backups to new Jenkins pod in {namespace} namespace... This might take a while.")
     run_command(f"kubectl -n {namespace} cp ./jenkins-backup.tar.gz {pod}:/tmp/jenkins-backup.tar.gz")
+    '''
     try:
         run_command(f"kubectl -n {namespace} cp ./jenkins-config-backup.tar.gz {pod}:/tmp/jenkins-config-backup.tar.gz")
     except subprocess.CalledProcessError:
         print("Config backup not found locally. Skipping config backup copy to pod.")
+    '''
 
+#def restore_jenkins_data(namespace, pod, backup_path, config_backup_path):
 def restore_jenkins_data(namespace, pod, backup_path, config_backup_path):
     print(f"Restoring backups in {namespace} namespace... This might take a while.")
-    run_command(f"kubectl -n {namespace} exec {pod} -- tar -xzvf /tmp/jenkins-backup.tar.gz -C {backup_path}")
+    run_command(f"kubectl -n {namespace} exec {pod} -- tar -xzvf /tmp/jenkins-backup.tar.gz --strip-components=1 -C {backup_path}")
+    '''
     try:
-        run_command(f"kubectl -n {namespace} exec {pod} -- tar -xzvf /tmp/jenkins-config-backup.tar.gz -C {config_backup_path}")
+        run_command(f"kubectl -n {namespace} exec {pod} -- tar -xzvf /tmp/jenkins-config-backup.tar.gz --strip-components=1 -C {config_backup_path}")
     except subprocess.CalledProcessError:
         print(f"Config backup not found on the pod. Skipping config restore.")
+    '''
 
 def main():
     if SWITCH_TENANT:
         switch_azure_ad(SOURCE_TENANT_ID)
 
     switch_kube_context(SOURCE_CONTEXT)
-    backup_jenkins_data(SOURCE_NAMESPACE, SOURCE_POD_NAME, "/var/jenkins_home", "/var/jenkins_config")
+    backup_jenkins_data(SOURCE_NAMESPACE, SOURCE_POD_NAME, "/var/jenkins_home")
+    #backup_jenkins_data(SOURCE_NAMESPACE, SOURCE_POD_NAME, "/var/jenkins_home", "/var/jenkins_config")
     copy_backups_to_local(SOURCE_NAMESPACE, SOURCE_POD_NAME)
 
     if SWITCH_TENANT:
@@ -76,7 +93,8 @@ def main():
 
     switch_kube_context(TARGET_CONTEXT)
     copy_backups_to_pod(TARGET_NAMESPACE, TARGET_POD_NAME)
-    restore_jenkins_data(TARGET_NAMESPACE, TARGET_POD_NAME, "/var/jenkins_home", "/var/jenkins_config")
+    restore_jenkins_data(TARGET_NAMESPACE, TARGET_POD_NAME, "/var/jenkins_home")
+    #restore_jenkins_data(TARGET_NAMESPACE, TARGET_POD_NAME, "/var/jenkins_home", "/var/jenkins_config")
 
     print("Migration completed successfully.")
 
