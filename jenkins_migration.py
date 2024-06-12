775f66f55d4e98c27585249482d3eacec4314b49
Zeeshan <zeeshanmedlari@gmail.com>
2024-06-12 13:26:15 +0530

    Final

--- jenkins_migration.py
+++ jenkins_migration.py
@@ -7,6 +7,7 @@ SOURCE_CONTEXT = "Argocd_Cluster"
 SOURCE_NAMESPACE = "jenkins"
 TARGET_CONTEXT = "jenkinsMigration_cluster"
 TARGET_NAMESPACE = "jenkins"
+CONTAINER_NAME = "jenkins"
 POD_NAME = "jenkins-0"
 SOURCE_TENANT_ID = "your-source-tenant-id"
 TARGET_TENANT_ID = "your-target-tenant-id"
@@ -30,36 +31,36 @@ def switch_kube_context(context):
     print(f"Switching to {context} cluster context...")
     run_command(f"kubectl config use-context {context}")
 
-def backup_jenkins_data(namespace, pod, backup_path):
+def backup_jenkins_data(namespace, pod, container, backup_path):
     print(f"Backing up Jenkins data from {namespace} namespace...")
-    run_command(f"kubectl -n {namespace} exec {pod} -- tar -czvf /tmp/jenkins-backup.tar.gz {backup_path}")
+    run_command(f"kubectl -n {namespace} exec {pod} -c {container} -- tar -czvf /tmp/jenkins-backup.tar.gz {backup_path}")
 
-def copy_backups_to_local(namespace, pod):
+def copy_backups_to_local(namespace, pod, container):
     print("Copying backups to local machine... This might take a while.")
-    run_command(f"kubectl -n {namespace} cp {pod}:/tmp/jenkins-backup.tar.gz ./jenkins-backup.tar.gz")
+    run_command(f"kubectl -n {namespace} cp {pod}:/tmp/jenkins-backup.tar.gz ./jenkins-backup.tar.gz -c {container}")
 
-def copy_backups_to_pod(namespace, pod):
+def copy_backups_to_pod(namespace, pod, container):
     print(f"Copying backups to new Jenkins pod in {namespace} namespace... This might take a while.")
-    run_command(f"kubectl -n {namespace} cp ./jenkins-backup.tar.gz {pod}:/tmp/jenkins-backup.tar.gz")
+    run_command(f"kubectl -n {namespace} cp ./jenkins-backup.tar.gz {pod}:/tmp/jenkins-backup.tar.gz -c {container}")
 
-def restore_jenkins_data(namespace, pod, backup_path):
+def restore_jenkins_data(namespace, pod, container, backup_path):
     print(f"Restoring backups in {namespace} namespace... This might take a while.")
-    run_command(f"kubectl -n {namespace} exec {pod} -- tar -xzvf /tmp/jenkins-backup.tar.gz --strip-components=1 -C {backup_path}")
+    run_command(f"kubectl -n {namespace} exec {pod} -c {container} -- tar -xzvf /tmp/jenkins-backup.tar.gz --strip-components=2 -C {backup_path}")
 
 def main():
     if SWITCH_TENANT:
         switch_azure_ad(SOURCE_TENANT_ID)
 
     switch_kube_context(SOURCE_CONTEXT)
-    backup_jenkins_data(SOURCE_NAMESPACE, POD_NAME, "/var/jenkins_home")
-    copy_backups_to_local(SOURCE_NAMESPACE, POD_NAME)
+    backup_jenkins_data(SOURCE_NAMESPACE, POD_NAME, CONTAINER_NAME, "/var/jenkins_home")
+    copy_backups_to_local(SOURCE_NAMESPACE, POD_NAME, CONTAINER_NAME)
 
     if SWITCH_TENANT:
         switch_azure_ad(TARGET_TENANT_ID)
 
     switch_kube_context(TARGET_CONTEXT)
-    copy_backups_to_pod(TARGET_NAMESPACE, POD_NAME)
-    restore_jenkins_data(TARGET_NAMESPACE, POD_NAME, "/var/jenkins_home")
+    copy_backups_to_pod(TARGET_NAMESPACE, POD_NAME, CONTAINER_NAME)
+    restore_jenkins_data(TARGET_NAMESPACE, POD_NAME, CONTAINER_NAME, "/var/jenkins_home")
 
     print("Migration completed successfully.")
 
